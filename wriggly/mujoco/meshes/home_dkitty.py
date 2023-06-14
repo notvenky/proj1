410b75e26141dea44d06235285e8d3801d864732
notvenky <venkyp2000@gmail.com>
2023-06-14 11:58:47 -0400

    working random sims

---
+++ wriggly/mujoco/meshes/home_dkitty.py
@@ -0,0 +1,81 @@
+import random
+import numpy as np
+import mujoco as mj
+
+
+def main():
+    max_width = 200
+    max_height = 200
+    # ctx = mj.GLContext(max_width, max_height)
+    # ctx.make_current()
+
+    cam = mj.MjvCamera()
+    opt = mj.MjvOption()
+
+    mj.glfw.glfw.init()
+    window = mj.glfw.glfw.create_window(1200, 900, "Demo", None, None)
+    mj.glfw.glfw.make_context_current(window)
+    mj.glfw.glfw.swap_interval(1)
+
+    mj.mjv_defaultCamera(cam)
+    mj.mjv_defaultOption(opt)
+
+    #xml_path = "/home/venky/Desktop/wriggly/simulation/meshes/wriggly.xml"
+    xml_path = "/home/venky/robel_dev/robel/robel-scenes/dkitty/kitty-v2.1.xml"
+    #xml_path = "/home/venky/Desktop/CILVR/quadruped/unitree/scene_torque.xml"
+
+
+    model = mj.MjModel.from_xml_path(xml_path)
+    data = mj.MjData(model)
+
+    scene = mj.MjvScene(model, maxgeom=10000)
+    context = mj.MjrContext(model, mj.mjtFontScale.mjFONTSCALE_150.value)
+
+    # Initialize goal positions
+    num_actuators = model.nu
+    goal_positions = [random.uniform(-1.57, 1.57) for _ in range(num_actuators)]
+
+    std_dev = 0.1  # Standard deviation of the Gaussian noise
+
+    while not mj.glfw.glfw.window_should_close(window):
+        simstart = data.time
+
+        while (data.time - simstart < 1.0 / 60.0):
+            # Update goal positions
+            for i in range(num_actuators):
+                # Check if the actuator has reached its goal position
+                if abs(data.ctrl[i] - goal_positions[i]) < 0.01:
+                    # Generate a new random goal position
+                    goal_positions[i] = random.uniform(-1.57, 1.57)
+
+                # Move the actuator towards the goal position
+                if data.ctrl[i] < goal_positions[i]:
+                    data.ctrl[i] += 0.01
+                else:
+                    data.ctrl[i] -= 0.01
+
+                # Add Gaussian noise
+                noise = np.random.normal(0, std_dev)
+                data.ctrl[i] += noise
+
+                #TODO: Cable Tension
+
+
+            mj.mj_step(model, data)
+
+        #viewport = mj.MjrRect(0, 0, 0, 0)
+        #mj.glfw.glfw.get_framebuffer_size(window)
+        viewport = mj.MjrRect(0, 0, 1200, 900)
+
+        #mj.mjv_updateScene(model, data, opt, None, cam, 0, scene)
+        mj.mjv_updateScene(model, data, opt, None, cam, mj.mjtCatBit.mjCAT_ALL.value, scene)
+        mj.mjr_render(viewport, scene, context)
+
+        mj.glfw.glfw.swap_buffers(window)
+        mj.glfw.glfw.poll_events()
+
+    mj.glfw.glfw.terminate()
+
+
+if __name__ == "__main__":
+    main()
--- wriggly/mujoco/meshes/pid_sim.py
+++ wriggly/mujoco/meshes/pid_sim.py
@@ -43,15 +43,42 @@ def main():
     simulation_speed = 0.5
     simulation_speed_ratio = 1
 
+    # Initialize a timer for each actuator
+    actuator_timers = np.zeros(model.nu)
+
+    # Random delay for each actuator
+    actuator_delays = np.random.randint(low=1, high=10, size=model.nu)
+
+    dxl_id = [0,1,2,3,4]
+
+    ANGLE_RANGES = {
+        0: (-1.57, 1.57),
+        1: (-3.14, 3.14),
+        2: (-1.57, 1.57),
+        3: (-3.14, 3.14),
+        4: (-1.57, 1.57)
+    }
+
+    #mapped_values = {id_value: ANGLE_RANGES[id_value] for id_value in dxl_id}
+
+    # PID control loop variables
+    KP = 0.5
+    KI = 0.1
+    KD = 0.1
+    current_positions = np.zeros(model.nu)
+    errors = np.zeros(model.nu)
+    last_errors = np.zeros(model.nu)
+    goal_positions = np.zeros(model.nu)
+
     while not mj.glfw.glfw.window_should_close(window):
         imgui.new_frame()
-
+        
         # Set window position and size
         imgui.set_next_window_position(50, 50, imgui.FIRST_USE_EVER)
         imgui.set_next_window_size(300, 200, imgui.FIRST_USE_EVER)
 
         imgui.begin("Simulation Speed", True)
-
+        
         # creating a slider for simulation speed
         changed, simulation_speed = imgui.slider_float("speed", simulation_speed, 0.1, 2.0)
         imgui.end()
@@ -61,7 +88,7 @@ def main():
 
         simstart = data.time
 
-        while (data.time - simstart < 1.0 / 60.0):
+        while (data.time - simstart < 1.0/60.0):
             # Count down each actuator's timer
             actuator_timers -= 1
 
@@ -74,26 +101,33 @@ def main():
             active_actuators = np.random.choice(np.where(mask)[0], size=min(5, sum(mask)), replace=False)
 
             # Generate random action commands for the selected actuators
+            # instead of setting a random goal position, we use a PID controller to generate it
             for dxl_id in active_actuators:
                 # Read present position (adapt to your simulation environment)
-                current_position = data.qpos[dxl_id]
-
+                current_positions[dxl_id] = data.qpos[dxl_id]
+                
                 # generate a random goal position
-                goal_position = random.uniform(-1.57, 1.57)
-
+                goal_positions[dxl_id] = random.uniform(ANGLE_RANGES[dxl_id][0], ANGLE_RANGES[dxl_id][1])
+                
                 # PID control
-                error = goal_position - current_position
-                control = error * simulation_speed
-
+                error = goal_positions[dxl_id] - current_positions[dxl_id]
+                errors[dxl_id] += error
+                derivative = error - last_errors[dxl_id]
+                control = KP * error + KI * errors[dxl_id] + KD * derivative
+                last_errors[dxl_id] = error
+                
                 # apply the control command to the actuator
                 data.ctrl[dxl_id] = control
 
             mj.mj_step(model, data)
             # Sleep for a while to slow down the simulation. Adjust the sleep duration as necessary.
-            # time.sleep((1.0/60.0) * simulation_speed_ratio)
+            #time.sleep((1.0/60.0) * simulation_speed_ratio)
 
+        #viewport = mj.MjrRect(0, 0, 0, 0)
+        #mj.glfw.glfw.get_framebuffer_size(window)
         viewport = mj.MjrRect(0, 0, 1200, 900)
 
+        #mj.mjv_updateScene(model, data, opt, None, cam, 0, scene)
         mj.mjv_updateScene(model, data, opt, None, cam, mj.mjtCatBit.mjCAT_ALL.value, scene)
         mj.mjr_render(viewport, scene, context)
 
@@ -101,6 +135,7 @@ def main():
         mj.glfw.glfw.poll_events()
 
     mj.glfw.glfw.terminate()
+
 
 if __name__ == "__main__":
     main()
