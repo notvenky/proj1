9727101801dc7362a5e5e4238b0bba25b40ef512
notvenky <venkyp2000@gmail.com>
2023-06-27 21:45:01 -0400

    homepos working

--- wriggly/hardware/control/home_pos.py
+++ wriggly/hardware/control/home_pos.py
@@ -1,5 +1,6 @@
 from config import *
 
+time.sleep(3)
 for i in range(len(DXL_ID_LIST)):
     dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID_LIST[i], ADDR_PRO_TORQUE_ENABLE, 0)
     if dxl_comm_result != COMM_SUCCESS:
