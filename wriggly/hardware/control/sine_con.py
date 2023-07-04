9a1b107964111739a9d7d15d278607b963ddcf6a
notvenky <venkyp2000@gmail.com>
2023-07-04 19:32:01 -0400

    sinecon edit

--- wriggly/hardware/control/sine_con.py
+++ wriggly/hardware/control/sine_con.py
@@ -274,18 +274,53 @@ try:
             goal_current = KP * error + KI * errors[dxl_id] + KD * derivative
             goal_position = current_positions[dxl_id] + KP * error + KI * errors[dxl_id] + KD * derivative
 
+        # Write goal current, torque and speed
+        dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, dxl_id, ADDR_PRO_GOAL_CURRENT, int(goal_current))
+        dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, dxl_id, ADDR_PRO_GOAL_SPEED, speed)
+        dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, dxl_id, ADDR_PRO_GOAL_POSITION, goal_position)
+        dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, dxl_id, ADDR_PRO_GOAL_CURRENT, torque)
+
+        write_counter += 1
+
+        # Write to log file every WRITE_FREQ iterations
+        if write_counter >= WRITE_FREQ:
+            # Reset the write counter
+            write_counter = 0
+
+            # Write to log file
+            timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+            with open('log.txt', 'a') as f:
+                f.write('{},{},{},{},{},{}\n'.format(timestamp, [actuator_ids], [directions], speed, torque))
+
+        # Read present position and current for all Dynamixels
+        for dxl_id in DXL_ID_LIST:
+            dxl_present_position, dxl_comm_result, dxl_error = packetHandler.read4ByteTxRx(portHandler, dxl_id, ADDR_PRO_PRESENT_POSITION)
+            if dxl_comm_result == COMM_SUCCESS:
+                current_positions[dxl_id] = dxl_present_position
+                dxl_present_current, dxl_comm_result, dxl_error = packetHandler.read4ByteTxRx(portHandler, dxl_id, ADDR_PRO_PRESENT_CURRENT)
+            if dxl_comm_result == COMM_SUCCESS:
+                pass
+
+        # Wait for keyboard interrupt or random time interval
+        try:
+            keyboard.wait('esc', suppress=True)
+            break
+        except:
+            pass
+    time.sleep(random.uniform(0.1, 0.5))
+
 except KeyboardInterrupt:
     pass
 
-    #Disable Dynamixel torque
-    for dxl_id in DXL_ID_LIST:
-        dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, dxl_id, ADDR_PRO_TORQUE_ENABLE, 0)
-        if dxl_comm_result != COMM_SUCCESS:
-            print(packetHandler.getTxRxResult(dxl_comm_result))
-        elif dxl_error != 0:
-            print(packetHandler.getRxPacketError(dxl_error))
-        else:
-            print("Dynamixel %d torque has been successfully disabled" % DXL_ID_LIST[i])
+#Disable Dynamixel torque
+for dxl_id in DXL_ID_LIST:
+    dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, dxl_id, ADDR_PRO_TORQUE_ENABLE, 0)
+    if dxl_comm_result != COMM_SUCCESS:
+        print(packetHandler.getTxRxResult(dxl_comm_result))
+    elif dxl_error != 0:
+        print(packetHandler.getRxPacketError(dxl_error))
+    else:
+        print("Dynamixel %d torque has been successfully disabled" % DXL_ID_LIST[i])
 
 #Close port
 portHandler.closePort()
