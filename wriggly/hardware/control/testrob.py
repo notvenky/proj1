19acf965b633c09d7ffbed8af5b441324b27d5bb
notvenky <venkyp2000@gmail.com>
2023-07-26 04:33:08 -0400

    testrob

---
+++ wriggly/hardware/control/testrob.py
@@ -0,0 +1,133 @@
+import numpy as np
+import time
+from dynamixel_sdk import *
+
+# Control table address
+ADDR_PRO_TORQUE_ENABLE = 64                  # Address for enabling the torque
+ADDR_PRO_GOAL_POSITION = 116                 # Address for goal position
+ADDR_PRO_PRESENT_POSITION = 132              # Address for present position
+ADDR_PRO_GOAL_CURRENT = 102                  # Address for goal current
+ADDR_PRO_PRESENT_CURRENT = 126               # Address for present current
+ADDR_PRO_GOAL_SPEED = 104                    # Address for speed
+ADDR_PRO_PRESENT_SPEED = 128
+ADDR_PRO_VELOCITY_TRAJECTORY = 136
+ADDR_PRO_POSITION_TRAJECTORY = 140
+
+# Define angle ranges for each Dynamixel
+ANGLE_RANGES = {
+    11: (1024, 3072),
+    12: (0, 4095),
+    20: (1024, 3072),
+    21: (1024, 3072),
+    22: (0, 4095)
+}
+
+
+MEAN_POSITION = 2048                          # Mean, starting position for all dynamixels 
+
+dxl_goal_position = [0,4095]
+JOYSTICK_THRESHOLD = 0.8
+DXL_MOVING_STATUS_THRESHOLD = 20
+
+# Protocol version
+PROTOCOL_VERSION = 2.0                        # 2.0 for XM430-W210
+
+# Default setting
+DXL_ID_LIST = [11, 12, 20, 21, 22]            # Dynamixel ID list
+BAUDRATE = 1000000                            # Dynamixel communication baudrate
+DEVICENAME = '/dev/ttyUSB0'                   # U2D2 USB-to-Serial converter device name
+
+# Initialize PortHandler instance
+portHandler = PortHandler(DEVICENAME)
+print(DXL_ID_LIST)
+# Initialize PacketHandler instance
+packetHandler = PacketHandler(PROTOCOL_VERSION)
+
+# Open port
+if portHandler.openPort():
+    print("Succeeded to open the port")
+else:
+    print("Failed to open the port")
+    quit()
+
+# Set port baudrate
+if portHandler.setBaudRate(BAUDRATE):
+    print("Succeeded to change the baudrate")
+else:
+    print("Failed to change the baudrate")
+    quit()
+
+
+for i in range(len(DXL_ID_LIST)):
+    # Enable Dynamixel Torque
+    dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID_LIST[i], ADDR_PRO_TORQUE_ENABLE, 1)
+    if dxl_comm_result != COMM_SUCCESS:
+        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+    elif dxl_error != 0:
+        print("%s" % packetHandler.getRxPacketError(dxl_error))
+    else:
+        print("Dynamixel %d has been successfully connected" % DXL_ID_LIST[i])
+
+    # Set initial position
+    dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, DXL_ID_LIST[i], ADDR_PRO_GOAL_POSITION, MEAN_POSITION)
+    if dxl_comm_result != COMM_SUCCESS:
+        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+    elif dxl_error != 0:
+        print("%s" % packetHandler.getRxPacketError(dxl_error))
+    else:
+        print("Dynamixel %d has been successfully set to initial position" % DXL_ID_LIST[i])
+
+# Define frequencies, amplitudes, and phases for each Dynamixel
+FREQUENCIES = {
+    11: 0.5,
+    12: 0.5,
+    20: 0.5,
+    21: 0.5,
+    22: 0.5
+}
+
+AMPLITUDES = {
+    11: 500,
+    12: 500,
+    20: 500,
+    21: 500,
+    22: 500
+}
+
+PHASES = {
+    11: 0,
+    12: 0,
+    20: 0,
+    21: 0,
+    22: 0
+}
+
+# Constants
+PI = 3.1416
+
+def oscillate_position(dxl_id, t):
+    """
+    Oscillates the position of the specified Dynamixel.
+    """
+    omega = 2 * PI * FREQUENCIES[dxl_id]
+    A = AMPLITUDES[dxl_id]
+    phi = PHASES[dxl_id]
+    
+    position = MEAN_POSITION + A * np.sin(omega * t + phi)
+
+    # Write the position
+    dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, dxl_id, ADDR_PRO_GOAL_POSITION, int(position))
+    if dxl_comm_result != COMM_SUCCESS:
+        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+    elif dxl_error != 0:
+        print("%s" % packetHandler.getRxPacketError(dxl_error))
+    else:
+        print("Dynamixel %d is oscillating at position: %d" % (dxl_id, position))
+
+# Oscillate each Dynamixel
+start_time = time.time()
+while True:
+    current_time = time.time() - start_time
+    for dxl_id in DXL_ID_LIST:
+        oscillate_position(dxl_id, current_time)
+    time.sleep(0.01)
