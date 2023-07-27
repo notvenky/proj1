a5f62b448aa3ee8feb4925789cbfef4373087b94
notvenky <venkyp2000@gmail.com>
2023-07-27 16:03:07 -0400

    mean changes

--- wriggly/hardware/control/oscillator/velcontrol.py
+++ wriggly/hardware/control/oscillator/velcontrol.py
@@ -27,8 +27,12 @@ out = cv2.VideoWriter(f'media_sin/video_{count}.avi', fourcc, 20.0, (1280, 480))
 
 amplitude_conversion_factor = 2048 / 3.14
 # paste_string = 'Frequency: tensor([0.4916, 0.2262, 0.4490, 0.4511, 0.3306]), Amplitude: tensor([1.5270, 1.4947, 0.8646, 0.8290, 1.4490]), Phase: tensor([0.7578, 2.0704, 0.4936, 5.0827, 1.1826])'
-paste_string = 'Frequency: tensor([0.4839, 0.3116, 0.1512, 0.4405, 0.0981]), Amplitude: tensor([1.2890, 2.8330, 0.9312, 2.8816, 0.0447]), Phase: tensor([1.9279, 1.2120, 0.1625, 0.4938, 5.1535])'
-# paste_string = 'Frequency: tensor([0.4254, 0.453, 0.1042, 0.1972, 0.4794]), Amplitude: tensor([1.0657, 1.8825, 1.4196, 0.289, 0.253]), Phase: tensor([0.6236, 0.3487, 5.1865, 4.6293, 2.9277])'
+# paste_string = 'Frequency: tensor([0.4839, 0.3116, 0.1512, 0.4405, 0.0981]), Amplitude: tensor([1.2890, 2.8330, 0.9312, 2.8816, 0.0447]), Phase: tensor([1.9279, 1.2120, 0.1625, 0.4938, 5.1535])'
+# bad - paste_string = 'Frequency: tensor([0.4254, 0.453, 0.1042, 0.1972, 0.4794]), Amplitude: tensor([1.0657, 1.8825, 1.4196, 0.289, 0.253]), Phase: tensor([0.6236, 0.3487, 5.1865, 4.6293, 2.9277])'
+
+paste_string = 'Frequency: tensor([0.4990, 0.2122, 0.4650, 0.4007, 0.3471]), Amplitude: tensor([0.9685, 2.2220, 1.4667, 0.7460, 0.9961]), Phase: tensor([5.4440, 2.9973, 2.8850, 3.7666, 5.2267])'
+
+
 
 COMMAND_FREQUENCY = 3.0
 COMMAND_PERIOD = 1.0 / COMMAND_FREQUENCY
