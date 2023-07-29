733baf4269138e45c3b64c50976ff13a0b8c4e75
notvenky <venkyp2000@gmail.com>
2023-07-29 01:52:30 -0400

    velcontrol

--- wriggly/hardware/control/oscillator/velcontrol.py
+++ wriggly/hardware/control/oscillator/velcontrol.py
@@ -30,8 +30,10 @@ amplitude_conversion_factor = 2048 / 3.14
 # paste_string = 'Frequency: tensor([0.4839, 0.3116, 0.1512, 0.4405, 0.0981]), Amplitude: tensor([1.2890, 2.8330, 0.9312, 2.8816, 0.0447]), Phase: tensor([1.9279, 1.2120, 0.1625, 0.4938, 5.1535])'
 # bad - paste_string = 'Frequency: tensor([0.4254, 0.453, 0.1042, 0.1972, 0.4794]), Amplitude: tensor([1.0657, 1.8825, 1.4196, 0.289, 0.253]), Phase: tensor([0.6236, 0.3487, 5.1865, 4.6293, 2.9277])'
 
-paste_string = 'Frequency: tensor([0.4990, 0.2122, 0.4650, 0.4007, 0.3471]), Amplitude: tensor([0.9685, 2.2220, 1.4667, 0.7460, 0.9961]), Phase: tensor([5.4440, 2.9973, 2.8850, 3.7666, 5.2267])'
-
+# paste_string = 'Frequency: tensor([0.4990, 0.2122, 0.4650, 0.4007, 0.3471]), Amplitude: tensor([0.9685, 2.2220, 1.4667, 0.7460, 0.9961]), Phase: tensor([5.4440, 2.9973, 2.8850, 3.7666, 5.2267])'
+# # vid23
+paste_string = 'Frequency: tensor([0.3772, 0.4522, 0.3793, 0.3916, 0.4428]), Amplitude: tensor([1.4870, 1.6738, 1.1526, 2.1733, 0.1395]), Phase: tensor([3.8298, 2.3467, 6.0578, 1.8011, 5.4534])'
+# first result from improved sim
 
 
 COMMAND_FREQUENCY = 3.0
